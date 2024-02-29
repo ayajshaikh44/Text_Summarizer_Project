bf3ae175a39d377c1664953bfe0ee706d701a54f
Ayaj Shaikh <ayajshaikh44@gmail.com>
2024-03-01 00:05:58 +0530

    Stage1 data ingestion is completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from text_summarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
 from text_summarizer.logging import logger
 
-logger.info("Welcome to our custom log")
+Stage_name = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>> stage {Stage_name} started <<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>> stage {Stage_name} completed <<<<\n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/text_summarizer/utils/common.py
+++ src/text_summarizer/utils/common.py
@@ -20,7 +20,9 @@ def read_yaml(path_to_yaml:Path)-> ConfigBox:
 
         Returns:
             ConfigBox: ConfigBox type
+
     """
+    print('a')
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
